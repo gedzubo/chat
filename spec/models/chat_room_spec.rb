e27764e03533f0cf7f49e5707f2793acb8de4aea
Gediminas Z <gediminas.zubovicius@gmail.com>
2025-02-27 09:59:19 +0000

    Add spec for the chat_room#member? method

--- spec/models/chat_room_spec.rb
+++ spec/models/chat_room_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe ChatRoom, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#member?" do
+    let(:chat_room) { create(:chat_room, user: user) }
+    let(:user) { create(:user) }
+
+    context "when the user is a member of the chat room" do
+      before do
+        chat_room.users << user
+      end
+
+      it "returns true" do
+        expect(chat_room.member?(user)).to be_truthy
+      end
+    end
+
+    context "when the user is not a member of the chat room" do
+      it "returns false" do
+        expect(chat_room.member?(user)).to be_falsey
+      end
+    end
+  end
 end
